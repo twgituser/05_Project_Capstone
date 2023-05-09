737a4d8bc2e2f896ae94e5bbebef8fcbae9f9e42
Terry Welch <terry_lee_welch@hotmail.com>
2023-05-09 17:47:07 +0100

    deploy with output

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,12 +4,12 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1: Create dockerpath
-dockerpath=twdockeruser/capstone-app
+dockerpath=twdockeruser/capstone-app:1.0
 
 # Step 2: Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 # cat .dh_token.txt | docker login --username=twdockeruser --password-stdin
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 # Step 3: Push image to a docker repository
-docker image tag capstone-app $dockerpath
+docker tag capstone-app $dockerpath
 docker image push $dockerpath
